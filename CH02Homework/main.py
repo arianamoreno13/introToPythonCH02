c5e556d0043d8438e90ce1f6b44371192d57ec91
ariana <ariana.moreno13@yahoo.com>
2023-07-08 17:36:33 -0600

    main.py added with code

---
+++ CH02Homework/main.py
@@ -0,0 +1,25 @@
+#Declare a variable and assign it the year you were born (or any number)
+birthYear = 1997
+#print the variable
+print(birthYear)
+#add 10 to the variable
+birthYear = birthYear + 10
+#print the variable
+print(birthYear)
+#subtract 100
+birthYear = birthYear - 100
+#print the variable
+print(birthYear)
+#multiply it by 2
+birthYear = birthYear * 2
+#print the variable
+print(birthYear)
+
+#Prompt the user for his/her name - use the input() function
+firstName = input("Enter your first name: ")
+#assign the name to a variable & print 
+print(f"Hello, {firstName}!")
+#capitalize it
+#print the variable
+#remove any heading or trailing spaces
+#print the variable
