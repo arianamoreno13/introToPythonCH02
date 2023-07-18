1ff33c0f369e971101d8923105a349e7dcc6bac1
ariana <ariana.moreno13@yahoo.com>
2023-07-18 17:37:49 -0600

    pylint updates have been made 10/10 score

--- Homework/while-loop.py
+++
@@ -1,7 +0,0 @@
-"""This is a while loop"""
-counter = 0
-
-while counter < 101:
-    print("counter: ", counter)
-
-    counter = counter + 2 
---
+++ Homework/while_loop.py
@@ -0,0 +1,7 @@
+"""This is a while loop"""
+COUNTER = 0
+
+while COUNTER < 101:
+    print("COUNTER: ", COUNTER)
+
+    COUNTER = COUNTER + 2
